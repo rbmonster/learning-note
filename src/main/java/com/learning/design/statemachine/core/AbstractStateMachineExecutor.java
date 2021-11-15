f122efa9131f5b3358a3b56591484052b2971c6d
sanwuhong <shanruhongx@163.com>
2021-11-15 10:14:02 +0800

    update network

--- src/main/java/com/learning/design/statemachine/core/AbstractStateMachineExecutor.java
+++ src/main/java/com/learning/design/statemachine/core/AbstractStateMachineExecutor.java
@@ -2,7 +2,7 @@ package com.learning.design.statemachine.core;
 
 public abstract class AbstractStateMachineExecutor<S extends Enum<S>, A extends Enum<A>, E extends Enum<E>, T, C extends StateMachineContext<S,T>> implements StateMachineExecutor<S, A, E, T, C> {
     @Override
-    final public EventTrigger<E> execute(Action<A> action, C executionContext) {
+    public final EventTrigger<E> execute(Action<A> action, C executionContext) {
         if (initPayload(executionContext)) {
             return doExecute(action, executionContext.getPayload());
         } else {
